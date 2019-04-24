b4210ad262f306af136e373b9c3e419cd8b5f129
tamlee9 <49918601+tamlee9@users.noreply.github.com>
2019-04-24 22:30:20 +0200

    Add files via upload

---
+++ EX1.py
@@ -0,0 +1,17 @@
+some_string="what's your mood today?"
+print(some_string)
+
+mood="happy"
+
+if mood=="happy":
+    print("it's great to see you happy!")
+elif mood=="nervous":
+    print("take a deep breath three times!")
+elif mood=="sad":
+    print("cheer up!")
+elif mood=="excited":
+    print("what are you so excited with?")
+elif mood=="relaxed":
+    print("keep that vibe!")
+
+
